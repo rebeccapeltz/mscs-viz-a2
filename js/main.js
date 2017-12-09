0bf03017776d2e9f7b2a1cb307016fa02a48e0b6
Rebecca Peltz <rebeccapeltz@gmail.com>
2017-12-09 08:56:26 -0800

    set up event handling

--- js/main.js
+++ js/main.js
@@ -1,31 +1,40 @@
 
 const yearIndex = {};
+const userTargetIndex = {};
 const nodes = [];
 const edges = [];
-let selectedYmo = '2011-9';
+
 // const colors = ['rgb(166,206,227)','rgb(31,120,180)','rgb(178,223,138)','rgb(51,160,44)','rgb(251,154,153)','rgb(227,26,28)','rgb(253,191,111)','rgb(255,127,0)','rgb(202,178,214)','rgb(106,61,154)'];
 const headers = ["SOURCE", "TARGET", "RATING", "TIME"];
 document.addEventListener("DOMContentLoaded", () => {
     let btnGo = document.querySelector("#btn-go");
-    btnGo.addEventListener("click", () => {
-    
-        let selectedMo =  document.querySelector('#mo option:checked').value;
-        $("#mo :selected").text()
-        let selectedYr = document.querySelector("#yr option:checked").value;
-        if (selectedMo === "0" || selectedYr === "0") {
-            alert("you must choose a month and a year");
-            return;
+
+    $.ajax({
+        type: "GET",
+        url: "rating1250.txt",
+        // url: "rating1new.txt",
+        // url: "soc-sign-bitcoinalpha.csv",
+        dataType: "text",
+        // success: function (data) { processData(data); }
+        success: function (data) {
+            processData(data);
         }
-        $.ajax({
-            type: "GET",
-            // url: "rating1250.txt",
-            // url: "rating1new.txt",
-            url: "soc-sign-bitcoinalpha.csv",
-            dataType: "text",
-            // success: function (data) { processData(data); }
-            success: function (data) { processData(data); }
-        });
+    });
+
+    document.addEventListener("userTargetFilter",(event)=>{
+        console.log(event);
+        processUserTargetFilter(event.detail.nodes);
+        renderChart();
+    });
+
+    btnGo.addEventListener("click", () => {
+        let selectedMo = document.querySelector('#mo option:checked').value;
+        let selectedYr = document.querySelector("#yr option:checked").value;
+        let selectedYmo = '2011-9';
+        processYmoFilter(selectedYmo);
+        renderChart();
     })
+
 });
 
 
@@ -50,10 +59,12 @@ class Edge {
     }
 }
 
+// process csv and create ymo and userTarget index
+
 function processData(allText) {
     let allTextLines = allText.split(/\r\n|\n/);
 
-    let year, month,ymo;
+    let year, month, ymo;
     for (let line of allTextLines) {
         // for (let i = 0; i < allTextLines.length; i++) {
         let data = line.split(',');
@@ -62,56 +73,33 @@ function processData(allText) {
         let date = new Date(data[3] * 1000);
         year = date.getFullYear();
         month = date.getMonth();
-        ymo =  `${year}-${month}`;
-        if (ymo === selectedYmo) {
+        ymo = `${year}-${month}`;
+        // if (ymo === selectedYmo) {
 
-            let obj = {};
-            obj[headers[0]] = data[0];
-            obj[headers[1]] = data[1];
-            obj[headers[2]] = data[2];
-            obj[headers[3]] = ymo;
+        let obj = {};
+        obj["SOURCE"] = data[0];
+        obj["TARGET"] = data[1];
+        obj["RATING"] = data[2];
+        obj["TIME"] = ymo;
 
-            if (!yearIndex[ymo]) {
-                yearIndex[ymo] = [];
-            }
-
-            yearIndex[ymo].push(obj);
-
+        if (!yearIndex[ymo]) {
+            yearIndex[ymo] = [];
         }
+        if (!userTargetIndex[obj["TARGET"]]) {
+            userTargetIndex[obj["TARGET"]] = [];
+        }
+
+        yearIndex[ymo].push(obj);
+        userTargetIndex[obj["TARGET"]].push(obj);
+        // }
     }
-    // for (let i = 0; i < allTextLines.length; i++) {
-    //     let data = allTextLines[i].split(',');
+    console.log(userTargetIndex);
+}
 
-    //     if (data.length == headers.length) {
-    //         year = new Date(data[3] * 1000).getFullYear();
-    //         if (year === selectedYear) {
-    //             let obj = {};
-    //             for (var j = 0; j < headers.length; j++) {
-    //                 if (j === 3) {
-    //                     obj[headers[j]] = year;
-    //                     // year = obj[headers[j]].getFullYear();
-    //                     console.log(year);
-    //                 } else {
-    //                     obj[headers[j]] = parseInt(data[j]);
-    //                 }
+function processYmoFilter(selectedYmo) {
 
-    //             }
-    //             if (!yearIndex[year]) {
-    //                 yearIndex[year] = [];
-    //             }
-
-    //             yearIndex[year].push(obj);
-
-    //         }
-
-
-
-    //     }
-    // }
-
-    console.log(yearIndex);
     let nodeIndex = {};
-    if (!yearIndex[selectedYmo]){
+    if (!yearIndex[selectedYmo]) {
         alert("no data for this time period");
         return;
     }
@@ -133,16 +121,45 @@ function processData(allText) {
     //console.table(nodes);
     // console.table(edges);
     // create a network
+}
+function processUserTargetFilter(selectedUser) {
 
+    let nodeIndex = {};
+    if (!userTargetIndex[selectedUser]) {
+        alert("no data for this target user");
+        return;
+    }
+    for (let user of userTargetIndex[selectedUser]) {
+        if (!nodeIndex[user.SOURCE]) {
+            let nodeObj = new Node(user.SOURCE, user.SOURCE);
+            nodes.push(nodeObj);
+            nodeIndex[user.SOURCE] = true;
+        }
+        if (!nodeIndex[user.TARGET]) {
+            let nodeObj = new Node(user.TARGET, user.TARGET);
+            nodes.push(nodeObj);
+            nodeIndex[user.TARGET] = true;
+        }
+
+        let edgeObj = new Edge(user.SOURCE, user.TARGET, user.RATING);
+        edges.push(edgeObj);
+    }
+    console.table(nodes);
+    // console.table(edges);
+    // create a network
+}
+
+function renderChart() {
+    document.getElementById("mynetwork").innerHTML="";
     let container = document.getElementById('mynetwork');
     let vizData = {
         nodes: nodes,
         edges: edges
     };
     let options = {
-        edges:{
+        edges: {
             arrows: {
-              to:     {enabled: true, scaleFactor:.5, type:'arrow'}
+                to: { enabled: true, scaleFactor: .5, type: 'arrow' }
             }
         },
         layout: {
@@ -152,5 +169,7 @@ function processData(allText) {
     let network = new vis.Network(container, vizData, options);
     network.on("selectNode", function (params) {
         console.log('selectNode Event:', params);
+        let event = new CustomEvent('userTargetFilter', { detail: params });
+        document.dispatchEvent(event);
     });
 }
